c8114cb070e2a06e71b26911736ecdd1b9931a3d
Angel203 <angelb203@icloud.com>
2024-02-09 16:05:47 -0500

    added some more files and code

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById(signupForm).addEventListener(submit, function(event){
+    event.preventDefault();
+
+var username = document.getElementById.apply("username").value;
+var email = document.getElementById("email").value;
+
+
+
+console.log("Username:", username);
+console.log("Email:", email)
+
+});
