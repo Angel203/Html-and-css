53dfec0a0ec1c5bf48078addbd07933831d52d56
Angel203 <angelb203@icloud.com>
2024-02-16 17:28:17 -0500

    added js syntax

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
-document.getElementById(signupForm).addEventListener(submit, function(event){
-    event.preventDefault();
+// document.getElementById(signupForm).addEventListener(submit, function(event){
+//     event.preventDefault();
+// function checkEmail(){
+//     let email = document.getElementById("email").value;
+//     let emailRegex = `user@example.com`
+    
+//     if (emailRegex.test(email)){
+//         document.getElementById("result").textContent = "Valid email address!";
+      
+//     } else {
+//         document.getElementById("result").textContent = "Sorry invalid email address!"
+//     }
+// }    
 
-var username = document.getElementById.apply("username").value;
-var email = document.getElementById("email").value;
+// let username = document.getElementById.apply("username").value;
+
+
+// console.log("Username:", username);
+// console.log("Email:", email)
+
+// });
 
 
 
-console.log("Username:", username);
-console.log("Email:", email)
+// 
 
-});
+let form = document.getElementById("form");
+myHeading.innerHTML = "Sign Up Here For more helpful information";
