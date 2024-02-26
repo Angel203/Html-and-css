2f60291fa7122a68b4a76435745db747bd1819ff
Angel203 <angelb203@icloud.com>
2024-02-26 12:27:30 -0500

    adding dropdown navbar

---
+++ nav.js
@@ -0,0 +1,19 @@
+// Function to toggle the visibility of the dropdown content
+function toggleDropdown() {
+    var dropdownContent = document.getElementById("myDropdown");
+    dropdownContent.classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      for (var i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+  
--- script.js
+++ script.js
@@ -1,28 +1,28 @@
-// document.getElementById(signupForm).addEventListener(submit, function(event){
-//     event.preventDefault();
-// function checkEmail(){
-//     let email = document.getElementById("email").value;
-//     let emailRegex = `user@example.com`
+document.getElementById(signupForm).addEventListener(submit, function(event){
+    event.preventDefault();
+function checkEmail(){
+    let email = document.getElementById("email").value;
+    let emailRegex = `user@example.com`
     
-//     if (emailRegex.test(email)){
-//         document.getElementById("result").textContent = "Valid email address!";
+    if (emailRegex.test(email)){
+        document.getElementById("result").textContent = "Valid email address!";
       
-//     } else {
-//         document.getElementById("result").textContent = "Sorry invalid email address!"
-//     }
-// }    
+    } else {
+        document.getElementById("result").textContent = "Sorry invalid email address!"
+    }
+}    
 
-// let username = document.getElementById.apply("username").value;
-
-
-// console.log("Username:", username);
-// console.log("Email:", email)
-
-// });
+let username = document.getElementById.apply("username").value;
 
 
+console.log("Username:", username);
+console.log("Email:", email)
 
-// 
+});
+
+
+
+uui-[]
 
 let form = document.getElementById("form");
 myHeading.innerHTML = "Sign Up Here For more helpful information";
